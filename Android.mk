b632abecd091ee480cc3f10628c2b7fc9eeab7e9
Ricardo Cerqueira <ricardo@cyngn.com>
2016-06-09 18:07:36 +0100

    skia: qc-perf: link against libqc-opt for msm8937/17/53
    Change-Id: If4e6297e64b0a9fcef9cb1eb7b2fba0e4474cf4f

--- Android.mk
+++ Android.mk
@@ -608,7 +608,7 @@ LOCAL_STATIC_LIBRARIES := \
 
 ifeq ($(TARGET_HAVE_QC_PERF),true)
     LOCAL_WHOLE_STATIC_LIBRARIES += libqc-skia
-    ifeq ($(call is-board-platform-in-list,msm8909 msm8916 msm8974 msm8992 msm8994 msm8996),true)
+    ifeq ($(call is-board-platform-in-list,msm8909 msm8916 msm8974 msm8992 msm8994 msm8996 msm8937 msm8917 msm8953),true)
         LOCAL_SHARED_LIBRARIES += libqc-opt
     endif
 endif
